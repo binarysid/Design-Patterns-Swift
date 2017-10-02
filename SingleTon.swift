44ade7298a5743650daa9c99ed385e7786c76b83
Linkon <siddique.linkon@gmailcom>
2017-10-02 15:27:03 +0600

    singleton pattern

---
+++ SingleTon.swift
@@ -0,0 +1,17 @@
+class SingleTon{
+    static var sharedInstance: SingleTon?
+    
+    static func getInstance()->SingleTon?{
+        guard let instance = sharedInstance else{
+            sharedInstance = SingleTon()
+            return sharedInstance
+        }
+        
+        return instance
+    }
+}
+
+let instanceA = SingleTon.getInstance()!
+print("object id A: \(String(ObjectIdentifier(instanceA).hashValue))")
+let instanceB = SingleTon.getInstance()!
+print("object id B: \(String(ObjectIdentifier(instanceB).hashValue))")

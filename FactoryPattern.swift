dfd187c5b8f72efa6e7f2ff8e5d1e1c21d227d75
Linkon <siddique.linkon@gmailcom>
2017-10-02 12:20:57 +0600

    first commit

---
+++ FactoryPattern.swift
@@ -0,0 +1,64 @@
+protocol Gun{
+    func getGunFeature()->String
+    
+}
+
+class Pistol: Gun{
+    var barrelLength:Float
+    
+    init(barrelLength: Float) {
+        self.barrelLength = barrelLength
+        
+    }
+    func getGunFeature()-> String{
+        
+        return "Pistol barrel length \(self.barrelLength) inch"
+    }
+}
+class Shotgun: Gun{
+    var barrelLength:Float
+    
+    init(barrelLength: Float) {
+        self.barrelLength = barrelLength
+        
+    }
+    func getGunFeature()-> String{
+        
+        return "Shotgun barrel length \(self.barrelLength) inch"
+    }
+}
+class Rifle: Gun{
+    var barrelLength:Float
+    
+    init(barrelLength: Float) {
+        self.barrelLength = barrelLength
+        
+    }
+    func getGunFeature()-> String{
+        
+        return "Rifle barrel length \(self.barrelLength) inch"
+    }
+}
+
+class Factory{
+    static func getGunFactory(shipType:String)->Gun?{
+        switch shipType {
+        case "P":
+            return Pistol(barrelLength: 15)
+            break
+        case "R":
+            return Rifle(barrelLength: 18)
+            break
+        case "S":
+            return Shotgun(barrelLength: 22)
+            break
+        default:
+            return nil
+            break
+        }
+        
+    }
+}
+
+let gun = Factory.getGunFactory(shipType: "R")
+print(gun!.getGunFeature())
